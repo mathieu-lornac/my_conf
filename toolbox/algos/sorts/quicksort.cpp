351061b698362c068b0e3f46d80195b4858a1e36
Mathieu Lornac <mathieu.lornac@astrium.eads.net>
2016-04-07 14:02:01 +0000

    Comments

--- toolbox/algos/sorts/quicksort.cpp
+++ toolbox/algos/sorts/quicksort.cpp
@@ -1,9 +1,12 @@
 // C++ 11 implementation of the quicksort
+
 // Pseudo algorithm
-// * to sort the array arr, pick an element called the pivot
-// * Run through the array in order to obtain all the elements lower or equal to the pivot to it's left
-// all the higher elements to it's right. idx is the index of the pivot
-// * Recursive calls on sub arrays until len of subarrays < 2
+// * Partition phase
+// ** Pick a pivot (-> in this imple, last element)
+// ** Reorganize the interval such that, the elements smaller are left to the pivot
+// the greater elements are to the right of the table
+// ** Pivot is at it's final place
+// * Recursive calls on sub arrays until len of subarrays <= 2
 
 #include <iostream>
 #include <stdlib.h>
@@ -22,6 +25,22 @@ void print_array(T lower, T upper) {
   std::cout << std::endl;
 }
 
+/**
+ * In place partitionning
+ * Returns iterator on the pivot position
+ * [lower elements][untreated][hole][higher elements]
+ * untreated[0] -> smaller
+ * untreated[-1] -> greater
+ * * Init phase => pivot is set to the last element of the array
+ *               greater points to "the hole"
+ * * Iteration until smaller != greater
+ *      current is set to the last untreated element
+ *      if current > pivot:
+ *          greater = current and --greater
+ *      else:
+ *          swap(first_untreated, current)
+ *          ++smaller
+*/
 template <class T>
 T partition(T itl, T itu) {
 
@@ -31,12 +50,10 @@ T partition(T itl, T itu) {
   while (smaller < greater) {
     auto current = greater - 1;
     if (*current > pivot) {
-      std::swap(*current, *greater);
+      *greater = *current;
       --greater;
     } else {
-      typename T::value_type tmp = *smaller;
-      *smaller = *current;
-      *current = tmp;
+      std::swap(*smaller, *current);
       ++smaller;
     }
   }
@@ -44,8 +61,15 @@ T partition(T itl, T itu) {
   return greater;
 }
 
-void quicksort(std::vector<int>::iterator itl,
-	       std::vector<int>::iterator itu) {
+/**
+ * In place quicksort
+ * Iterators call
+ * itl -> first element of the collection
+ * itu -> last element of the collection (not collection.end(), must point to the last element)
+ */
+template<class T>
+void quicksort(T itl,
+	       T itu) {
   if ((itu - itl) < 1) {
     return;
   } else if ((itu - itl) == 1) {
@@ -55,11 +79,23 @@ void quicksort(std::vector<int>::iterator itl,
     return;
   }
   auto &&pivot = partition(itl, itu);
-  std::future<void> lowerSort = std::async(std::launch::async, std::bind(quicksort, itl, pivot - 1));
-  std::future<void> upperSort = std::async(std::launch::async, std::bind(quicksort, pivot + 1, itu));
+  std::future<void> lowerSort = std::async(std::launch::async, std::bind(quicksort<T>, itl, pivot - 1));
+  std::future<void> upperSort = std::async(std::launch::async, std::bind(quicksort<T>, pivot + 1, itu));
   lowerSort.wait();
   upperSort.wait();
 }
+
+/**
+ * In place quicksort
+ * Collection call
+ */
+template<class T>
+void quicksort(T &array) {
+  typename T::iterator itl = array.begin();
+  typename T::iterator itu = array.end();
+  quicksort<typename T::iterator>(itl, itu - 1);
+}
+
 
 int main() {
 
@@ -68,14 +104,13 @@ int main() {
 
   std::cout << "Array to sort:";
   // Array initialisation
-  for (int i = 0; i < 10 + rand() % 10; ++i) {
+  for (int i = 0; i < rand() % 20; ++i) {
     array.push_back(rand() % 100);
     std::cout << " " << array.back();
   }
   std::cout << std::endl;
 
-  quicksort(array.begin(), array.end() - 1);
-
+  quicksort(array);
   std::cout << "Sorted array:";
   for (auto && v : array) {
     std::cout << " " << v;
